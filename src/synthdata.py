aa51722f5a29c3612193bb9073b48179cb71ee7f
Andrew Whitamore <product@palama-technologies.com>
2025-07-16 12:12:52 +0100

    M-step works correctly

--- src/synthdata.py
+++ src/synthdata.py
@@ -1,25 +1,26 @@
 import numpy as np
 
-def generate_synthetic_weights(M, use_flag):
+def generate_synthetic_weights(X, M, mask_gen):
     
     # ... sample weights from a Gaussian distribution
     w = np.random.multivariate_normal(np.zeros(M), np.eye(M))
     
-    # ... set weights to zero for basis functions being omitted
-    w = np.multiply(w, use_flag)
+    # ... apply the basis function mask so that we use only the selected basis functions
+    w_gen = w[mask_gen]
+    X_gen = X[:, mask_gen]
     
-    return w
+    return w_gen, X_gen
 
-def generate_synthetic_data(X, w, emission_variance_generative):
+def generate_synthetic_data(X_gen, w_gen, emission_variance_generative):
     
-    # ... dimensions
-    N, M = X.shape
+    # ... dimensions of the reduced-dimension design matrix X_gen
+    N, _ = X_gen.shape
     
     # ... random noise
     emission_st_dev_generative = np.sqrt(emission_variance_generative)
     emission_noise = np.random.normal(0, emission_st_dev_generative, size = N)
     
     # ... synthetic data
-    synth_data = np.dot(X, w) + emission_noise
+    synth_data = np.dot(X_gen, w_gen) + emission_noise
     
     return synth_data
